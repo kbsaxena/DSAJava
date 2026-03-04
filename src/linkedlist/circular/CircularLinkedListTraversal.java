73ae4814e925e48e6e762741127b3bb76c03806b
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-03-04 11:46:28 +0530

    Added Few More files

--- src/linkedlist/circular/CircularLinkedListTraversal.java
+++ src/linkedlist/circular/CircularLinkedListTraversal.java
@@ -10,7 +10,17 @@ class Node {
 }
 
 public class CircularLinkedListTraversal {
-    void printList(Node head) {
+    void printList1(Node head) {
+        // Print head first
+        System.out.print(head.data + " ");
+        Node temp = head.next;
+        while(temp != head){
+            System.out.print(temp.data + "");
+            temp = temp.next;
+        }
+    }
+
+    void printList2(Node head) {
         // Your code here
         Node temp = head;
         while(temp.next != head){
