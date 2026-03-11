f5094e6f86d8a5670484bb1551ed5b1ecd514b2e
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-03-11 20:19:49 +0530

    Added Few More files

---
+++ src/binarytrees/BinaryTreeNode.java
@@ -0,0 +1,33 @@
+package binarytrees;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+    }
+}
+public class BinaryTreeNode {
+    public static void main(String[] args) {
+        Node a = new Node(3);
+        Node b = new Node(5);
+        Node c = new Node(2);
+        Node d = new Node(7);
+        Node e = new Node(1);
+        Node f = new Node(6);
+        a.left = b; a.right = c;
+        b.left = d; b.right = e;
+        c.right = f;
+
+        print(a);
+    }
+
+    private static void print(Node root) {
+        //base case
+        if(root == null) return;
+        System.out.print(root.data + " ");
+        print(root.left);
+        print(root.right);
+    }
+}
