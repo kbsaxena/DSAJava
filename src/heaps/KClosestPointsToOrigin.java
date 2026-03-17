014668cc84b121a58add09f17c205e7f9cc77d72
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-03-17 09:13:58 +0530

    Added Few More files

--- src/heaps/KClosestPointsToOrigin.java
+++ src/heaps/KClosestPointsToOrigin.java
@@ -4,23 +4,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-class Triplet implements Comparable<Triplet>{
-    int dist;
-    int x;
-    int y;
-    Triplet(int dist, int x, int y){
-        this.dist = dist;
-        this.x = x;
-        this.y = y;
+public class KClosestPointsToOrigin {
+    public class Triplet implements Comparable<Triplet>{
+        int dist;
+        int x;
+        int y;
+        Triplet(int dist, int x, int y){
+            this.dist = dist;
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Triplet t) {
+            //return this.dist-t.dist;
+            return Integer.compare(this.dist, t.dist);
+        }
     }
 
-    @Override
-    public int compareTo(Triplet t) {
-        //return this.dist-t.dist;
-        return Integer.compare(this.dist, t.dist);
-    }
-}
-public class KClosestPointsToOrigin {
     public ArrayList<ArrayList<Integer>> kClosest(int[][] points, int k) {
         // code here
         PriorityQueue<Triplet> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
