cb80a73c6e95f5746258547cf3bb88f4867099d8
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-03-22 12:01:46 +0530

    Added Few More files

---
+++ src/graphs/CourseSchedule1.java
@@ -0,0 +1,39 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CourseSchedule1 {
+    public boolean canFinish(int n, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        int[] in_degree = new int[n];
+        for(int i = 0; i < prerequisites.length; i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adjList.get(u).add(v);
+            //adjList.get(v).add(u); // It is directed graph so no reverse link
+
+            in_degree[v]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(in_degree[i]==0) q.add(i);
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(q.size()>0){
+            int vertex = q.remove();
+            ans.add(vertex);
+            for(int ele: adjList.get(vertex)){
+                in_degree[ele]--;
+                if(in_degree[ele] == 0) q.add(ele);
+            }
+        }
+        return (n==ans.size());
+    }
+}
