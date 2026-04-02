a0c2379a2891200414e9c2f740cd4f2a07d51c6a
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-04-02 12:38:52 +0530

    Added few more files

--- src/sorting/advanced/quicksort/QuickSort.java
+++ src/sorting/advanced/quicksort/QuickSort.java
@@ -8,6 +8,7 @@ public class QuickSort {
         quickSort(arr,pivot+1,hi);
     }
 
+    //With last element as pivot
     private int partition(int[] arr, int lo, int hi) {
         int pivot = arr[hi];
         int i=lo, j=hi-1;
@@ -26,5 +27,54 @@ public class QuickSort {
         arr[hi] = temp;
 
         return i;
+    }
+
+    private int partitionWithFirstElementPivot(int[] arr, int lo, int hi) {
+        int pivot = arr[lo];
+        int i = lo+1, j = hi;
+        while(i<=j){
+            if(arr[i]<=pivot) i++;
+            else if(arr[j]>pivot) j--;
+            else{
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        //swap first(lo) with j
+        int temp = arr[j];
+        arr[j] = arr[lo];
+        arr[lo] = temp;
+
+        return j;
+    }
+
+    private static int partitionWithMiddleElementPivot(int[] arr, int lo, int hi) {
+        int mid = lo + (hi - lo) / 2;
+
+        // Move mid element to start (lo)
+        int temp = arr[mid];
+        arr[mid] = arr[lo];
+        arr[lo] = temp;
+
+        int pivot = arr[lo];
+        int i = lo + 1, j = hi;
+
+        while (i <= j) {
+            if (arr[i] <= pivot) i++;
+            else if (arr[j] > pivot) j--;
+            else {
+                int t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
+            }
+        }
+
+        // Place pivot at correct position
+        int t = arr[j];
+        arr[j] = arr[lo];
+        arr[lo] = t;
+
+        return j;
     }
 }
