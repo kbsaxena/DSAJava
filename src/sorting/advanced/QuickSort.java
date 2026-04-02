167f43f37ca3f63d14c4298cd65077713782656f
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-04-02 11:47:30 +0530

    Stack Files

---
+++ src/sorting/advanced/QuickSort.java
@@ -0,0 +1,30 @@
+package sorting.advanced;
+
+public class QuickSort {
+    public void quickSort(int[] arr, int lo, int hi) {
+        if(lo>=hi) return;
+        int pivot = partition(arr,lo,hi);
+        quickSort(arr,lo,pivot-1);
+        quickSort(arr,pivot+1,hi);
+    }
+
+    private int partition(int[] arr, int lo, int hi) {
+        int pivot = arr[hi];
+        int i=lo, j=hi-1;
+        while(i<=j){
+            if(arr[i]<=pivot) i++;
+            else if(arr[j]>pivot) j--;
+            else{
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        //swap last(hi) with i
+        int temp = arr[i];
+        arr[i] = arr[hi];
+        arr[hi] = temp;
+
+        return i;
+    }
+}
