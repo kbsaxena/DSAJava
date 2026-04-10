9eb94f69e837a3de413884183862022d3e29fdd0
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-04-10 20:50:58 +0530

    Added few more files

--- src/dp/MinimumCostClimbingStairs.java
+++ src/dp/MinimumCostClimbingStairs.java
@@ -1,8 +1,23 @@
 package dp;
 
+import java.util.Arrays;
+
 public class MinimumCostClimbingStairs {
-    //Using Simple Recursion
+    //Method 2 Using DP
     static int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+        Arrays.fill(dp, -1);
+        return Math.min(minCost(cost, 0, dp), minCost(cost, 1, dp));
+    }
+
+    private static int minCost(int[] cost, int i, int[] dp) {
+        if(i>=cost.length) return 0;
+        if(dp[i] != -1) return dp[i];
+        return dp[i] = cost[i] + Math.min(minCost(cost, i+1, dp), minCost(cost,i+2, dp));
+    }
+
+    //Method 1: Using Simple Recursion
+    static int minCostClimbingStairs1(int[] cost) {
         return Math.min(minCost(cost, 0), minCost(cost, 1));
     }
 
