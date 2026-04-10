8c081c1ab6acb838ffe8555a678e8b43e7a2eb13
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-04-10 22:03:55 +0530

    Added few more files

---
+++ src/dp/UniquePaths.java
@@ -0,0 +1,30 @@
+package dp;
+
+public class UniquePaths {
+    //Method 2 Using DP
+    public int numberOfPaths(int m, int n) {// m: m to 1, n: n to 1
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                dp[i][j] = -1;
+            }
+        }
+        return paths(m, n, dp);
+    }
+
+    public int paths(int m, int n, int[][] dp){
+        if(m == 1 || n == 1) return 1;
+        if(dp[m][n] != -1) return dp[m][n];
+        return dp[m][n] = paths(m-1,n, dp) + paths(m,n-1, dp);
+    }
+
+    //Method 1 Simple Recursion
+    public int numberOfPaths1(int m, int n) {
+        return paths(m,n);
+    }
+
+    public int paths(int m, int n){
+        if(m == 1 || n == 1) return 1;
+        return paths(m-1,n) + paths(m,n-1);
+    }
+}
