3665b98f909e38b97dc90832768f021fd00509db
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-04-12 21:30:40 +0530

    Added few more files

--- src/dp/UniquePaths.java
+++ src/dp/UniquePaths.java
@@ -1,8 +1,33 @@
 package dp;
 
 public class UniquePaths {
-    //Method 3 Using Tabulation
+    //Method 4 Using Tabulation Space Optimized
     public int numberOfPaths(int m, int n) {
+        int[][] dp = new int[2][n];
+
+        // Step 1: initialize first row
+        for(int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // Step 2: build rows one by one
+        for(int i = 1; i < m; i++) {
+            dp[1][0] = 1; // first column always 1
+
+            for(int j = 1; j < n; j++) {
+                dp[1][j] = dp[0][j] + dp[1][j-1];
+            }
+
+            // Step 3: copy current row to previous row
+            for(int j = 0; j < n; j++) {
+                dp[0][j] = dp[1][j];
+            }
+        }
+
+        return dp[0][n-1];
+    }
+    //Method 3 Using Tabulation
+    public int numberOfPaths3(int m, int n) {
         int[][] dp = new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
