3f2e4af11881c11589063ac7c86304a33aa59d71
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-04-16 12:11:03 +0530

    Added few more files

--- src/dp/CoinChange_MinimumCoins.java
+++ src/dp/CoinChange_MinimumCoins.java
@@ -3,8 +3,42 @@ package dp;
 import java.util.Arrays;
 
 public class CoinChange_MinimumCoins {
-    //Method 2 Dp Memoization
+    //Method 3 Dp Tabulation
     public int minCoins(int coins[], int sum) {
+        int n = coins.length;
+        long[][] dp = new long[n][sum+1];
+
+        // 🔹 Base case (i == n)
+        /*
+        if(i == coins.length){ //n
+            if(sum == 0) return 0;
+            else return Integer.MAX_VALUE;
+        }
+         */
+        for (int s = 0; s <= sum; s++) {
+            if (s == 0) dp[n][s] = 0;
+            else dp[n][s] = Integer.MAX_VALUE;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j=0;j<=sum;j++) {
+                //Copied and pasted all code from minimumCoins method except base case
+                if(j-coins[i]>=0){
+                    long pick = 1 + dp[i][j-coins[i]];
+                    long skip = dp[i+1][j];
+                    dp[i][j] = Math.min(pick, skip);
+                } else{
+                    long skip = dp[i+1][j];
+                    dp[i][j] = skip;
+                }
+            }
+        }
+        long ans = dp[0][sum];
+        return (ans >= Integer.MAX_VALUE) ? -1: (int)ans;
+    }
+
+    //Method 2 Dp Memoization
+    public int minCoins2(int coins[], int sum) {
         int n = coins.length;
         long[][] dp = new long[n][sum+1];
         for (long[] a: dp) {
