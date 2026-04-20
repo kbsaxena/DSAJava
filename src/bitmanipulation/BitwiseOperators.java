58228ae8b2e2142ed2d0174937331f9cf0cfb0c1
Kulbhushan Saxena <kulbhushan.saxena09@gmail.com>
2026-04-20 12:29:17 +0530

    Added few more files

---
+++ src/bitmanipulation/BitwiseOperators.java
@@ -0,0 +1,9 @@
+package bitmanipulation;
+
+public class BitwiseOperators {
+    public static void main(String[] args) {
+        System.out.println(49&29); //Bitwise AND => 17
+        System.out.println(49|29); //Bitwise OR => 61
+        System.out.println(49^29); //Bitwise XOR => 44
+    }
+}
